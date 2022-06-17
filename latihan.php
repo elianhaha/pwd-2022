d278cfd1659f9a3cb880053a98f79c8c3f332df3
adhitya <20410100069@dinamika.ac.id>
2022-06-17 17:56:37 +0700

    menambahkan file baru

---
+++ latihan.php
@@ -0,0 +1,26 @@
+<!-- php : server side (mengelola)-->
+<!-- cara menulis syntax php -->
+<?php
+//echo "Hello World";
+//membuat variabel
+//1. diawali dengan simbol dollar
+//2. tidak boleh diawali dengan angka dan tidak ada spasi
+$nama = "Adhitya";
+var_dump($nama);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba PHP</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
