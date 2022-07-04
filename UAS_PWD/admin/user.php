395ce428268623dc6acfaacc2c0d8a6c012bfe91
adhitya-arya <106930133+adhitya-arya@users.noreply.github.com>
2022-07-04 07:10:12 +0700

    kereta api

--- UAS_PWD/admin/user.php
+++ UAS_PWD/admin/user.php
@@ -4,6 +4,6 @@ $connect = mysqli_connect('localhost', 'root', '', 'test');
 function delete($id)
 {
     global $connect;
-    mysqli_query($connect, "DELETE FROM register WHERE id = '$id'");
+    mysqli_query($connect, "DELETE FROM users WHERE id = '$id'");
     return mysqli_affected_rows($connect);
 }
