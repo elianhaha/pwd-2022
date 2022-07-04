14ac351db36605d029e29939ba28a14c970e44e5
adhitya-arya <106930133+adhitya-arya@users.noreply.github.com>
2022-07-04 07:13:34 +0700

    kereta api

--- UAS_PWD/user/navbar.php
+++ UAS_PWD/user/navbar.php
@@ -4,7 +4,7 @@
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#"><img src="download.png" alt="kereta api"></a>
+        <a class="navbar-brand" href="index.php"><img src="download.png" alt="kereta api"></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
